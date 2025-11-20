1ae3d844986e7ac15639220f405d57201d909934
splurf <ejsch03@gmail.com>
2025-11-20 09:06:16 -0500

    refac, restruct shared values into one Arc

--- src/lib.rs
+++ src/lib.rs
@@ -17,15 +17,19 @@ pub struct Tuning {
 }
 
 impl Tuning {
-    const DEFAULT_TUNING: Tuning = Tuning {
+    pub const DEFAULT: Tuning = Tuning {
         busy_spin: Duration::from_micros(8),
         fast_spin: Duration::from_micros(128),
         fast_chunk: Duration::from_micros(32),
     };
 
     /// Create a tuning configuration with default latency-oriented values.
-    pub const fn new() -> Self {
-        Self::DEFAULT_TUNING
+    pub const fn new(busy_spin: Duration, fast_spin: Duration, fast_chunk: Duration) -> Self {
+        Self {
+            busy_spin,
+            fast_spin,
+            fast_chunk,
+        }
     }
 
     /// Set the maximum duration of the initial pure spin phase.
@@ -49,24 +53,29 @@ impl Tuning {
 
 impl Default for Tuning {
     fn default() -> Self {
-        Self::new()
+        Self::DEFAULT
     }
+}
+
+struct Inner {
+    counter: AtomicU64,
+    parked: AtomicBool,
+    thread: Mutex<Thread>,
 }
 
 /// Sends notifications.
 #[derive(Clone)]
 pub struct UnitSender {
-    inner: Arc<AtomicU64>,
-    thread: Arc<Mutex<Thread>>,
-    parked: Arc<AtomicBool>,
+    inner: Arc<Inner>,
 }
 
 impl UnitSender {
     pub fn send(&self) {
-        self.inner.fetch_add(1, Ordering::Release);
+        self.inner.counter.fetch_add(1, Ordering::Release);
+
         // only unpark if needed
-        if self.parked.load(Ordering::Relaxed) {
-            self.thread.lock().unpark();
+        if self.inner.parked.load(Ordering::Relaxed) {
+            self.inner.thread.lock().unpark();
         }
     }
 }
@@ -74,21 +83,20 @@ impl UnitSender {
 /// Receives notifications.
 #[derive(Clone)]
 pub struct UnitReceiver {
-    inner: Arc<AtomicU64>,
-    next_ticket: Arc<AtomicU64>,
-    sleeper: SpinSleeper,
-    thread: Arc<Mutex<Thread>>,
-    parked: Arc<AtomicBool>,
+    inner: Arc<Inner>,
+    next: Arc<AtomicU64>,
+    spinner: SpinSleeper,
 }
 
 impl UnitReceiver {
+    /// update the shared [`Thread`] with the current thread.
     pub fn update_thread(&self) {
-        *self.thread.lock() = current()
+        *self.inner.thread.lock() = current()
     }
 
-    /// mitigate [`Ordering::Acquire`] spam
+    /// mitigate [`Ordering::Acquire`] spam.
     fn fence(&self, target: u64) -> bool {
-        if self.inner.load(Ordering::Relaxed) >= target {
+        if self.inner.counter.load(Ordering::Relaxed) >= target {
             std::sync::atomic::fence(Ordering::Acquire);
             true
         } else {
@@ -97,72 +105,80 @@ impl UnitReceiver {
     }
 
     /// recv with explicit tuning (pure spin -> spin-sleep -> park).
-    pub fn recv_with_tuning(&self, tuning: Tuning) {
+    pub fn recv_with_tuning(
+        &self,
+        Tuning {
+            busy_spin,
+            fast_spin,
+            fast_chunk,
+        }: Tuning,
+    ) {
         // which event index this recv() is waiting for
-        let target = self.next_ticket.fetch_add(1, Ordering::Relaxed);
+        let target = self.next.fetch_add(1, Ordering::Relaxed);
 
-        // phase 0: pure spin
+        // phase 1: busy wait
         let pure_start = Instant::now();
-        while pure_start.elapsed() < tuning.busy_spin {
+        while pure_start.elapsed() < busy_spin {
             if self.fence(target) {
                 return;
             }
             spin_loop();
         }
 
-        // phase 1: spin-sleep
+        // phase 2: spin-sleep
         let start_spinsleep = Instant::now();
-        while start_spinsleep.elapsed() < tuning.fast_spin {
+        while start_spinsleep.elapsed() < fast_spin {
             if self.fence(target) {
                 return;
             }
-            self.sleeper.sleep(tuning.fast_chunk);
+            self.spinner.sleep(fast_chunk);
         }
 
-        // phase 2: park
+        // phase 3: park
         loop {
             if self.fence(target) {
                 return;
             }
 
             // mark as parked before re-checking
-            self.parked.store(true, Ordering::Relaxed);
+            self.inner.parked.store(true, Ordering::Relaxed);
 
             // re-check after marking parked to avoid missing a send
             if self.fence(target) {
-                self.parked.store(false, Ordering::Relaxed);
+                self.inner.parked.store(false, Ordering::Relaxed);
                 return;
             }
 
             // continue with parking
             std::thread::park();
-            self.parked.store(false, Ordering::Relaxed);
+            self.inner.parked.store(false, Ordering::Relaxed);
         }
     }
 
     /// recv using the default tuning parameters.
     pub fn recv(&self) {
-        self.recv_with_tuning(Tuning::DEFAULT_TUNING);
+        self.recv_with_tuning(Tuning::DEFAULT);
     }
 }
 
 pub fn unit_channel() -> (UnitSender, UnitReceiver) {
-    let inner: Arc<AtomicU64> = Default::default();
-    let thread = Arc::new(Mutex::new(current()));
-    let parked: Arc<AtomicBool> = Default::default();
+    let inner = Arc::new(Inner {
+        counter: Default::default(),
+        parked: Default::default(),
+        thread: Mutex::new(current()),
+    });
 
     let sender = UnitSender {
         inner: inner.clone(),
-        thread: thread.clone(),
-        parked: parked.clone(),
     };
+
+    let next = Arc::new(AtomicU64::new(1));
+    let spinner = SpinSleeper::default();
 
     let receiver = UnitReceiver {
         inner,
-        next_ticket: Arc::new(AtomicU64::new(1)),
-        sleeper: SpinSleeper::default(),
-        thread,
-        parked,
+        next,
+        spinner,
     };
 
     (sender, receiver)
